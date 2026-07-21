e69e8cde80f402558d511b2ae3e3ff23cf661b3b
Sharath <24eg105v27@anurag.edu.in>
2026-07-21 15:26:54 +0530

    fixing ✅

---
+++ server/src/main/java/jar/dto/Sumdto.java
@@ -0,0 +1,18 @@
+package jar.dto;
+
+public class Sumdto {
+   public String msg="sum api end point";
+   public int status=200;
+   public int number=0;
+   public int sumofn = 0;
+   public void setNumber(int n){
+        this.number=n;
+
+    }
+    public void setSumofN(int x){
+        this.sumofn=x;
+
+    }
+
+
+}
